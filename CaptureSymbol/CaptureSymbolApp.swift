89a58ae74b422fffbbc97258aaedbef1853a72eb
Sebastian Yanni <sebasyanni@gmail.com>
2024-07-05 14:36:40 -0300

    Initial Commit

---
+++ CaptureSymbol/CaptureSymbolApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureSymbolApp.swift
+//  CaptureSymbol
+//
+//  Created by Sebastian Yanni on 05/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CaptureSymbolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
